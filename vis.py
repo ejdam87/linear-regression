3f9b9b2172c18087b3a795880428cc2385a749aa
ejdam87 <adam.dzadon@gmail.com>
2023-02-07 13:49:40 +0100

    Add photos

--- vis.py
+++ vis.py
@@ -18,6 +18,8 @@ def vectorify( *args: int ) -> Vector:
 def show_2d_regression( data: list[ Sample ],
                         model: LinearRegression ) -> None:
     
+    assert model.dim == 1
+
     xs = [ x[ 0 ] for x, _ in data ]
     ys = [ y for _, y in data ]
 
@@ -31,6 +33,8 @@ def show_2d_regression( data: list[ Sample ],
 def show_3d_regression( data: list[ Sample ],
                         model: LinearRegression ) -> None:
     
+    assert model.dim == 2
+
     xs = [ x[ 0 ] for x, _ in data ]
     ys = [ x[ 1 ] for x, _ in data ]
     zs = [ z for _, z in data ]
@@ -38,12 +42,13 @@ def show_3d_regression( data: list[ Sample ],
     fig = plt.figure()
     ax = fig.add_subplot( projection="3d" )
 
-    ax.scatter( xs, ys, zs )
+    ax.scatter( xs, ys, zs, color="b" )
 
     zs = [ model.approximate( vectorify( x, y ) ) for x, y in zip( xs, ys ) ]
-    ax.plot_trisurf( xs, ys, zs )
+    ax.plot_trisurf( xs, ys, zs, color="g" )
     plt.show( )
 
 model = LinearRegression( 2 )
-dataset = datagen.create_dataset( 11, 2 )
+dataset = datagen.create_dataset( 15, 2 )
+model.learn( dataset )
 show_3d_regression( dataset, model )
